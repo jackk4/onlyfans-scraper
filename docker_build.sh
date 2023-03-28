f4084fb43a081e3d554ae6825ea5c2923b555c1d
Jackie Jack Jackson <jackwalsh44@proton.me>
2023-03-28 11:35:56 -0400

    Fixing docker build release logic

--- docker_build.sh
+++ docker_build.sh
@@ -5,7 +5,7 @@ DEFAULT_VERSION="latest"
 
 X_TAG_NAME=$(git branch --show-current)
 
-if [[ "$X_TAG_NAME" =~ ^release/[0-9]+\./[0-9]+\./[0-9]+$ ]]; then
+if [[ "$X_TAG_NAME" =~ ^release\/[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     TAG_NAME=${X_TAG_NAME}
 else
     echo Unable to create a tag from branch name ["${X_TAG_NAME}"]. Make sure to only run this from a release branch.
