036f40a8fa8127131590fd0ae790d80a08e4f0e0
jackk4 <129111031+jackk4@users.noreply.github.com>
2023-03-28 19:25:28 -0400

    Fixing docker build on AWS (#8)
    * Fixing docker build on AWS
    
    * Fixing for AWS
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DOCKER_REPO="$DOCKER_REPO"
 DOCKERFILE="onlyfans_scraper/dist/Dockerfile"
 IMAGE_NAME="of-scraper"
 DEFAULT_VERSION="latest"
@@ -12,9 +13,12 @@ if [[ "$BRANCH_NAME" =~ ^release\/[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 
 else
     echo Unable to create a tag from branch name ["${X_TAG_NAME}"]. Make sure to only run this from a release branch.
-    exit
+    TAG_NAME=${DEFAULT_VERSION}
+    echo Using 'latest'...
 fi
 
 docker build \
-    --tag "${IMAGE_NAME}:${TAG_NAME}" \
+    --tag "${DOCKER_REPO}/${IMAGE_NAME}:${TAG_NAME}" \
     --file "${DOCKERFILE}" .
+
+docker push ${DOCKER_REPO}/${IMAGE_NAME}:${TAG_NAME}
--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name of-scraper -it -v /home/ubuntu/scraped-data:/scraper/onlyfans_scraper/runtime of-scraper:0.0.3
+docker run --name of-scraper -it -v /home/ubuntu/scraped-data:/scraper/onlyfans_scraper/runtime $DOCKER_REPO/of-scraper:0.0.4
