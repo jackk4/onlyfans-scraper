b0973d0c074aa907703dcf16fa52054579e7b210
jackk4 <129111031+jackk4@users.noreply.github.com>
2023-03-28 19:41:46 -0400

    Moving pip install to dockerfile (#10)

--- onlyfans_scraper/dist/run.sh
+++ onlyfans_scraper/dist/run.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-python -m pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
 python -m onlyfans_scraper.scraper "$@"
