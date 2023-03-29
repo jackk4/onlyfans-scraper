7029f84c7d19a4ac41c9bc329cf7acec39d52e9a
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2023-03-29 01:46:29 +0000

    Renaming main branch

---
+++ docker_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if ! [[ $1 =~ [0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	echo Please input a version in the format MajorVersion.MinorVersion.PatchVersion.
+	exit
+fi
+
+docker run --name of-scraper -it -v /home/ubuntu/scraped-data:/scraper/onlyfans_scraper/runtime $DOCKER_REPO/of-scraper:$1 $2
